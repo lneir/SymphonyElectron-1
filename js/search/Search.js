112ccf69b7f3999840e69d5a5035f562754794b8
serkanmulayim <serkanmulayim@gmail.com>
2017-04-25 17:12:29 -0700

    searcher

--- js/search/Search.js
+++ js/search/Search.js
@@ -1,4 +1,8 @@
 'use strict';
+const childProcess = require('child_process');
+const PWD = 'pwd';
+const SEARCHBIN = 'demosearcher';
+const SEARCHARGS = ['0', '0000000000000', '9999999999999']
 
 class Search {
 
@@ -7,11 +11,20 @@ class Search {
     }
 
     query(query) {
-        return new Promise((resolve, reject) => {
-            resolve({
-                text: 'hello world'
-            });
+      return new Promise((resolve, reject) => {
+        //var child = childProcess.execFile(PWD,[],(error,stdout,stderr) => {
+        console.log('searching for args:' + query.toString());
+        var child = childProcess.execFile(SEARCHBIN,query,(error,stdout,stderr) => {
+          if(error) {
+            console.error('stderr',stderr);
+            throw error;
+          }
+
+          console.log(stdout);
+          //resolve(query);
+          resolve(JSON.parse(stdout));
         });
+      });
     }
 }
 
